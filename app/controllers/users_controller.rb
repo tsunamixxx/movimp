6f48268c28963d8adfef891001262309843cf52b
津波忍 <tsuna@tsunamishinobu-no-MacBook-Air.local>
2018-01-17 21:15:18 +0900

    avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    # order(created_at: :desc)で新しい投稿順にする
+    # order(created_at: :desc)で新しい投稿順にしたいが、投稿順のまま
     @reviews = Review.all.order(created_at: :desc)
     @user = User.find(params[:id])
   end
