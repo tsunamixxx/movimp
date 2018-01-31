2bc3c99e569e5f006889777c36a248d043058ef6
津波忍 <tsuna@tsunamishinobu-no-MacBook-Air.local>
2018-01-31 11:33:21 +0900

    ユーザー詳細画面で最新記事を上にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    # order(created_at: :desc)で新しい投稿順にしたいが、投稿順のまま
-    @reviews = Review.all.order(created_at: :desc)
     @user = User.find(params[:id])
   end
 
